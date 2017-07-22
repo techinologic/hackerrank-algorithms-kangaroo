12ba7c9772dd0116fe3b323506fbe4246ae79c6e
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-07-22 15:47:14 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        int dist = x1 - x2;
+        int dv = v1 - v2;
+        if ((dv * dist < 0) && (dist % dv == 0))
+            return "YES";
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+    }
+}
+
